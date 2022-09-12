cf650436af88058b1d8b046a726af5113b594ea8
Юрий Аверьянов <averianovj@gmail.com>
2022-09-12 22:28:43 +0300

    first commit

---
+++ src/main/java/task/Main.java
@@ -0,0 +1,9 @@
+package task;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "11"));
+        System.out.println(bins.mult("10", "11"));
+    }
+}
